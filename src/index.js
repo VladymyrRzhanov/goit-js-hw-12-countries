a7dcb77a285fcf25e0e412ac95407b837f43da83
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-05-29 21:29:30 +0300

    заменил дебаунс, фунцию рендера

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const debounce = require('lodash.debounce');
+import debounce from 'lodash.debounce';
 import callError from "./js/error";
 import countryListTpl from "./templates/country-list.hbs";
 import countryCardTpl from "./templates/country-card.hbs";
 import API from "./js/fetchCountries";
 import getRefs from "./js/refs";
+import './sass/main.scss';
 
 const refs = getRefs();
 
 const renderCountry = (country) => {
-    if (country.length > 2 && country.length < 10) {
+    if (country.length >= 2 && country.length <= 10) {
         refs.outputCountry.innerHTML = countryListTpl(country);
         
     } else if (country.length === 1) {
+
         refs.outputCountry.innerHTML = countryCardTpl(...country);
         
     } else {
@@ -25,7 +27,7 @@ function searchCountry(e) {
     this.value = this.value.replace(/[^\[A-Za-zА]/g, '');
     const searchQuery = e.target.value;
     if(searchQuery.length) {
-        API.fetchCountries(searchQuery).then(renderCountry);
+        API.fetchCountries(searchQuery).then(renderCountry).catch(callError);
     };
 };
 
